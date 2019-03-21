2efc7a8042a554def5f247e1bd967a87eba389c5
Christian <chris_1693@yahoo.com>
2019-03-21 17:47:48 -0400

    client - libraries

---
+++ client.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include <termios.h>
+#include <fcntl.h>
+#include <sys/time.h>
+
+#include <time.h>
+#include <signal.h>
+#include <stdbool.h>
+
+#define MAXLINE 1024
