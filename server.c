12e1e4d6626b7791e01c376b5b94d4ef66b3e5f0
Christian <chris_1693@yahoo.com>
2019-03-21 16:27:12 -0400

    SERVER adding libreries to work sockets

---
+++ server.c
@@ -0,0 +1,8 @@
+#include <sys/socket.h>       
+#include <sys/types.h>        
+#include <arpa/inet.h>        
+#include <unistd.h>           
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
